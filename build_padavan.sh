81f58c40cffc22e23fc98c011820babb080845c1
st-ty1 <stephan.tratzky@gmail.com>
2021-11-06 17:20:16 +0100

    upload patches and script

---
+++ build_padavan.sh
@@ -0,0 +1,26 @@
+#! /bin/sh
+
+PADAVAN_PATCHES_DIR=$HOME/Artix_padavan
+PADAVAN_REPO_DIR=$HOME/padavan-ng
+
+cd $PADAVAN_REPO_DIR
+git clean -dxf 
+git reset --hard
+git pull
+git checkout master
+
+cd toolchain
+./build_toolchain.sh
+
+# replace config-template for your needs; here for example Asus RT-N14U is used
+cp -v $PADAVAN_REPO_DIR/configs/templates/asus/rt-n14u.config $PADAVAN_REPO_DIR/trunk/.config
+
+#patches for Artix
+patch -i $PADAVAN_PATCHES_DIR/Makefile_util-linux.patch $PADAVAN_REPO_DIR/trunk/user/util-linux/Makefile
+patch -i $PADAVAN_PATCHES_DIR/Makefile_libtirpc.patch $PADAVAN_REPO_DIR/trunk/libs/libtirpc/Makefile
+#patch -i $PADAVAN_PATCHES_DIR/Makefile_tor.patch $PADAVAN_REPO_DIR/trunk/user/tor/Makefile
+patch -i $PADAVAN_PATCHES_DIR/Makefile_libsqlite.patch $PADAVAN_REPO_DIR/trunk/libs/libsqlite/Makefile
+patch -i $PADAVAN_PATCHES_DIR/configure.ac_transmission.patch $PADAVAN_REPO_DIR/trunk/user/transmission/transmission-3.0X/configure.ac
+
+cd trunk
+./build_firmware.sh
